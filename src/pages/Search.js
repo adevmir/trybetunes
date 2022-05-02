1a974ae8896c3aeca9155c473ea1ef3dc5207410
adevmir <ademirvalonqar@gmail.com>
2022-05-02 18:34:01 -0300

    Requisito 05

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,56 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.onInputChange = this.onInputChange.bind(this);
+    this.state = {
+      search: '',
+    //  loading: false,
+    };
+  }
+
+  onInputChange({ target }) {
+    const { name } = target;
+    this.setState({
+      [name]: target.value,
+    });
+  }
+
+  isSearchButtonDisabled = () => {
+    const {
+      search,
+    } = this.state;
+    const minCaracter = 2;
+    if (search.length >= minCaracter) {
+      return false;
+    }
+    return true;
+  }
+
   render() {
+    const {
+      search,
+    } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         search
+        <input
+          name="search"
+          type="text"
+          data-testid="search-artist-input"
+          value={ search }
+          onChange={ this.onInputChange }
+        />
+        <button
+          name="SearchButton"
+          type="submit"
+          data-testid="search-artist-button"
+          disabled={ this.isSearchButtonDisabled() }
+        >
+          Pesquisar
+        </button>
       </div>
     );
   }
